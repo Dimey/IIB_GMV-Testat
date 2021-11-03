56d760d8a7e275aff25d567c9d5524c43d7754e7
Dimey <dimi.haas@googlemail.com>
2021-11-03 09:40:06 +0100

    batch import further implementations

--- gmvtestatcontroller.py
+++ gmvtestatcontroller.py
@@ -55,7 +55,7 @@ class TestatController():
             print('Die MoodleListe konnte nicht geladen werden.')
 
     def oeffneKPOrdner(self):
-        path = self.view.folderDialog()
+        path = self.view.folderDialog() + '/'
         if path:
             self.view.zeigeLadenHaken(self.view.BatchImportKpLaden_btn)
-            self.model.ladeBatch()
+            self.model.ladeBatch(path)
--- gmvtestatmodel.py
+++ gmvtestatmodel.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+import os
 
 class TestatData():
     def __init__(self):
@@ -21,3 +23,24 @@ class TestatData():
         teilnehmerliste = teilnehmerliste.reindex(columns = columns_titles)
         teilnehmerliste[['Punkte','Bestanden']] = ''
         self.bewertungsuebersicht = teilnehmerliste
+
+    def ladeBatch(self, path):
+        konstruktionsprotokolleListe = []
+        for foldername in os.listdir(path):
+            if not foldername.startswith('.'):
+                for filename in os.listdir(path + foldername):
+                    if filename.endswith('html'):
+                        kp = pd.read_html(f"{path+foldername}/{filename}")[0].to_numpy()
+                        konstruktionsprotokolleListe.append(kp)
+                        # verkettete xml erstellen
+                        # kp.to_xml(f"konstruktionsprotokolle.xml",index=False,root_name=f"id{filename[0:-5]}")
+                        # werte KP aus
+                    else:
+                        print('Nicht passend.')
+        
+        #kpDF = pd.DataFrame(konstruktionsprotokolleListe)
+        print(konstruktionsprotokolleListe)
+
+    def bepunkteKP(self, kp):
+        # return: punkte
+        pass
